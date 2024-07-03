0d9b4a4e68d0aaa1f2989a0336acdcea61f5fe82
mdunggggg <dung.hm@idtinc.co>
2024-07-03 15:58:36 +0700

    Initial Commit

---
+++ TodoSwiftUi/TodoSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftUiApp.swift
+//  TodoSwiftUi
+//
+//  Created by ByDuo on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
